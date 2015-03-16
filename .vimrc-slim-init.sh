4ee3014d03df0225be7f0a110d357efc08d51fe6
Will Durney <willdurney@gmail.com>
2015-03-16 18:28:25 -0400

    Add vimrc-slim-init script

---
+++ .vimrc-slim-init.sh
@@ -0,0 +1,8 @@
+git clone https://github.com/futurityverb/dotfiles.git ~/.futurityverb-dotfiles
+mkdir ~/.vim
+mkdir ~/.vim/bundle
+cp ~/.futurityverb-dotfiles/.vimrc-slim ~/.vimrc
+git clone https://github.com/kien/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
+git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+git clone https://github.com/scrooloose/syntastic.git ~/.vim/bundle/syntastic
+git clone https://github.com/Lokaltog/vim-easymotion.git ~/.vim/bundle/vim-easymotion
