5d573183f34cf56bf8f83d2344ec1f6dd30dc55c
Will Durney <willdurney@gmail.com>
2015-03-16 18:41:34 -0400

    Adjust shell script

--- vimrc-slim-init.sh
+++ vimrc-slim-init.sh
@@ -1,7 +1,6 @@
-git clone https://github.com/futurityverb/dotfiles.git ~/.futurityverb-dotfiles
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-cp ~/.futurityverb-dotfiles/.vimrc-slim ~/.vimrc
+cp .vimrc-slim ~/.vimrc
 git clone https://github.com/kien/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 git clone https://github.com/scrooloose/syntastic.git ~/.vim/bundle/syntastic
