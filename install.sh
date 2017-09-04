15c3a94b345dada8a9bfb7aae20428c5292448b1
Will Durney <willdurney@gmail.com>
2017-09-04 12:31:52 -0400

    Add install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ln -sf ~/dotfiles/.elite50-phpcs-ruleset.xml ~/.elite50-phpcs-ruleset.xml
+ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
+ln -sf ~/dotfiles/.jshintrc ~/.jshintrc
+ln -sf ~/dotfiles/.scss-lint.yml ~/.scss-lint.yml
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
+
+echo Successfully created dotfile symlinks
--- install_vimrc_slim.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-rm -rf ~/.vim/autoload/pathogen.vim
-rm -rf ~/.vim/bundle
-rm -rf ~/.vim/colors
-curl -fLo ~/.vimrc https://raw.githubusercontent.com/futurityverb/dotfiles/master/.vimrc-slim
-vim +PlugInstall +PlugUpdate +qall!
